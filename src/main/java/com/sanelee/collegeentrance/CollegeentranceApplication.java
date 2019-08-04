2fe18f4aef7ce73f2c4984919fcbb1cbc1ccd8ee
sanelee38 <1870731037@163.com>
2019-08-04 10:34:35 +0800

    init repo

---
+++ src/main/java/com/sanelee/collegeentrance/CollegeentranceApplication.java
@@ -0,0 +1,13 @@
+package com.sanelee.collegeentrance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeentranceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollegeentranceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sanelee/collegeentrance/CollegeentranceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sanelee.collegeentrance;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CollegeentranceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
