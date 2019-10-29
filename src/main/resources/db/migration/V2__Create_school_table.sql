dcc1fb54c3623b6869743627a39930fd8f09126b
sanelee38 <1870731037@163.com>
2019-10-29 12:04:23 +0800

    修改SQL语句

--- src/main/resources/db/migration/V2__Create_school_table.sql
+++ src/main/resources/db/migration/V2__Create_school_table.sql
@@ -1,12 +1,13 @@
 create table school
 (
-    scid int auto_increment
+    scid        int auto_increment
         primary key,
-    name varchar(50) null,
-    areaid int null,
-    batch varchar(20) null,
-    description longtext null,
-    acronym varchar(10) null,
+    name        varchar(50) null,
+    areaname    varchar(20) null,
+    areaid      int         null,
+    batch       varchar(20) null,
+    description longtext    null,
+    acronym     varchar(10) null,
     constraint school_ibfk_1
         foreign key (areaid) references area (aid)
 );
