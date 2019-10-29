f6cb10633f91fe16e7d6b92e21d416bd2ecdfccd
sanelee38 <1870731037@163.com>
2019-10-29 11:56:59 +0800

    插入数据

---
+++ src/main/resources/db/migration/V12__Add_sort.sql
@@ -0,0 +1,2 @@
+insert into `sort` (`sid`, `sort`) values('1','理科');
+insert into `sort` (`sid`, `sort`) values('2','文科');
---
+++ src/main/resources/db/migration/V16__Add_2017score.sql
@@ -0,0 +1,59 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.5.40
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','2','6','陕西','623','591','573','8935','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','3','6','陕西','609','508','487','32391','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','11','6','陕西','609','558','531','17750','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','12','6','陕西','675','642','627','2624','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','13','6','陕西','652','614','599','5303','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','14','6','陕西','562','519','504','25879','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','15','6','陕西','496','463','455','48592','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','16','6','陕西','539','460','450','51522','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','17','6','陕西','534','480','472','39327','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','18','6','陕西','510','455','449','52134','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','19','6','陕西','601','560','538','16004','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','20','6','陕西','577','537','524','19624','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','21','6','陕西','552','451','438','59024','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','22','6','陕西','610','555','542','15034','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','23','6','陕西','565','528','434','61652','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','24','6','陕西',NULL,'434','427','66288','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','25','6','陕西','467','427','421','70218','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','26','6','陕西','457','423','417','72924','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','27','6','陕西',NULL,'417','413','75608','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','28','6','陕西','569','502','479','35942','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','29','6','陕西',NULL,NULL,'437','59665','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','30','6','陕西',NULL,'413','409','78348','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','31','6','陕西',NULL,NULL,NULL,NULL,'1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','32','6','陕西',NULL,NULL,NULL,NULL,'1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','33','6','陕西',NULL,'438','432','62909','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','34','6','陕西',NULL,'410','399','85204','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','35','6','陕西',NULL,'407','404','81752','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','36','6','陕西','452','412','408','79027','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','37','6','陕西','402','356','337','121942','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','38','6','陕西','530','451','443','55804','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','39','6','陕西','568','497','483','34083','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','40','6','陕西','476','424','417','72924','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','41','6','陕西',NULL,'470','444','55163','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','42','6','陕西',NULL,'369','349','115600','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','43','6','陕西',NULL,'356','337','121942','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','44','6','陕西',NULL,'353','330','125356','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','45','6','陕西',NULL,'362','344','118306','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','46','6','陕西',NULL,'341','326','127267','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','47','6','陕西',NULL,'347','324','128227','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','48','6','陕西',NULL,'322','309','134878','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','49','6','陕西','402','343','326','127267','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','50','6','陕西','458','405','398','85875','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','51','6','陕西',NULL,'391','384','94945','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','52','6','陕西',NULL,'404','397','86503','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','53','6','陕西',NULL,NULL,NULL,NULL,'1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','54','6','陕西',NULL,'359','342','119407','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','55','6','陕西',NULL,'367','352','113955','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','56','6','陕西','409','382','370','103666','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','57','6','陕西',NULL,NULL,NULL,NULL,'1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','58','6','陕西',NULL,'403','397','86503','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','59','6','陕西',NULL,'360','342','119407','1');
+insert into `2017score` (`sort`, `scid`, `aid`, `area`, `maxscore`, `avgscore`, `minscore`, `minrank`, `sid`) values('理科','1','6','陕西','526','472','449','52134','1');
