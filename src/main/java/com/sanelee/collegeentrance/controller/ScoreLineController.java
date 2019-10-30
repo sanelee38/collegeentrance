98196056b604caeaf84aef2dca0705b1c45edd28
sanelee38 <1870731037@163.com>
2019-10-30 11:31:21 +0800

    注册scoreline页面

--- src/main/java/com/sanelee/collegeentrance/controller/ScoreLineController.java
+++ src/main/java/com/sanelee/collegeentrance/controller/ScoreLineController.java
@@ -7,9 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
 public class ScoreLineController {
+    @RequestMapping("/scoreline")
+    public String register(){
+        return "scoreline";
+    }
 }
